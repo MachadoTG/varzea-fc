9778b2c46863aa07a166978dcd17d13b47e60d0b
GustavoTM <gtmmtg.gm@gmail.com>
2023-04-06 12:00:57 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'varzea-sc',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
